2a4f9e565a481e4088d25691e97915c3352f712a
Nome do usuário <email@dominio.com>
2023-03-08 16:52:23 -0300

    confirmar

---
+++ src/components/Counter.js
@@ -0,0 +1,51 @@
+import { Component } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native"
+export default class Counter extends Component {
+    state = {
+        count: 0,
+        step: 2,
+    };
+    incrementar() {
+      this.setState({
+        count: this.state.count + this.state.step,
+      });
+    }
+    decrementar() {
+        if (this.state.count > this.props.min || this.props.min == undefined) {
+            this.setState({
+                count: this.state.count - 1,
+            });
+        }
+    }
+    alteraStep(text) {
+      this.setState({ step: parseInt(text) });
+    }
+    render() {
+        return (
+          <View style={styles.contador}>
+            <TextInput
+              value={this.state.step.toString()}
+              onChange={(e) => {
+                this.alteraStep(e.nativeEvent.text);
+              }}
+            />
+            <TextInput
+              defaultValue={this.state.step.toString()}
+              onChangeText={(valor) => {
+                this.alteraStep(valor);
+              }}
+            />
+            <Text>Contador: {this.state.count}</Text>;
+            <Button onPress={() => this.incrementar()} title="+" />
+            <Button onPress={() => this.decrementar()} title="+" />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    contador: {
+        flexdirection: 'row',
+        alignItems: 'center',
+    },
+});
